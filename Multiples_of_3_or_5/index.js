6977d3ef3a0f3fa33a42b9b811cf7cb57e376a2e
mahdita97 <taala.mahdy@gmail.com>
2021-12-28 17:00:00 +0330

    feat: add problem_1 new solution
    and also little mistake in first solution

--- Multiples_of_3_or_5/index.js
+++ Multiples_of_3_or_5/index.js
@@ -2,12 +2,27 @@
 
 // Find the sum of all the multiples of 3 or 5 below 1000.
 
-function calc(number_input) {
+// Method 1
+function calc(highest_number) {
   let result = 0;
-  for (let i = 0; i < number_input.length; i++) {
+  for (let i = 0; i < highest_number; i++) {
     if (i % 3 === 0 || i % 5 === 0) {
       result = result + i;
     }
   }
   return result;
+}
+
+// Method 2
+function calc(highest_number) {
+  const number_one = Math.floor(highest_number / 3) * 3;
+  const temp1 = Math.floor(highest_number / 3) * ((3 + number_one) / 2);
+
+  const number_two = Math.floor(highest_number / 5) * 5;
+  const temp2 = Math.floor(highest_number / 5) * ((5 + number_two) / 2);
+
+  const number_three = Math.floor(highest_number / 15) * 15;
+  const temp3 = Math.floor(highest_number / 15) * ((15 + number_three) / 2);
+
+  return temp1 + temp2 - temp3;
 }
